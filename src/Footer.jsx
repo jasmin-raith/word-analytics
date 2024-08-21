45c9e218c88074dbeafeb3e556734119e5913d2c
Jasmin Raith <jasminraith@icloud.com>
2024-08-21 15:36:02 +0200

    add first structure to main container and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,8 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>Last checked limits: 17 days ago</small>
+    </footer>
+  );
 }
