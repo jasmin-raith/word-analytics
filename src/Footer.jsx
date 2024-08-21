1655cf42bd76224a457cddda93ffcdbd99b05801
Jasmin Raith <jasminraith@icloud.com>
2024-08-21 16:01:18 +0200

    add styling

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
       <small>Last checked limits: 17 days ago</small>
     </footer>
